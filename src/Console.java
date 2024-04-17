683c05bec9fd851ffd5f0040516246d0559e85cd
Jujej <jpdurau@gmail.com>
2024-04-16 21:19:49 -0300

    Console certo

---
+++ src/Console.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Console {
+
+    private static Scanner scan = new Scanner(System.in); // o private bloqueia que outra classe consiga acessar
+
+    public static int lerInt() {
+        int valor = scan.nextInt();
+        scan.nextLine();
+        return valor;
+    }
+
+    public static String lerString() {
+        String txt = scan.nextLine();
+        return txt;
+    }
+
+    public static float lerFloat() {
+        float valor = scan.nextFloat();
+        scan.nextLine();
+        return valor;
+    }
+
+}
